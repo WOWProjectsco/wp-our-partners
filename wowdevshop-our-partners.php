0d502b99224f28ebc3e82d4c5d35fdd19b3dd181
XicoOfficial <xicoofficial@gmail.com>
2016-01-07 21:48:41 -0600

    Add basic file structure - Add header on plugin *¨.php files - Add default banner and icon for wowdevshop plugins

---
+++ wowdevshop-our-partners.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+* Plugin Name: Our Partners
+* Plugin URI: http://wowdevshop.com
+* Description: This plugin registers the 'partner' post type, it let's you manage your company partner profiles.
+* Author: XicoOfficial
+* Version: 1.0.0
+* License: GPLv2
+* Author URI: http://wowdevshop.com
+* Text Domain: our-partners-by-wowdevshop
+*
+* @package WordPress
+* @subpackage WowDevShop_Our_Partners
+* @author XicoOfficial
+* @since 1.0.0
+*/
+
+
+?>
