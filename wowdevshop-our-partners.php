4a5c741e3ccced36ae3dc0d349b731e5dc82ea26
XicoOfficial <xicoofficial@gmail.com>
2016-01-07 23:55:01 -0600

    Add save custom data function

--- wowdevshop-our-partners.php
+++ wowdevshop-our-partners.php
@@ -163,4 +163,38 @@ wp_nonce_field( basename( __FILE__ ), 'partner_fields_nonce' );
     echo '</table>'; // end table
 }
 
+//
+// Save the Data
+//
+add_action('save_post', 'save_custom_meta');
+
+function save_custom_meta($post_id) {
+    global $custom_meta_fields;
+
+    // verify nonce
+    if (!isset($_POST['partner_fields_nonce']) || !wp_verify_nonce($_POST['partner_fields_nonce'], basename(__FILE__)))
+        return $post_id;
+    // check autosave
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+        return $post_id;
+    // check permissions
+    if ('page' == $_POST['post_type']) {
+        if (!current_user_can('edit_page', $post_id))
+            return $post_id;
+        } elseif (!current_user_can('edit_post', $post_id)) {
+            return $post_id;
+    }
+
+    // loop through fields and save the data
+    foreach ($custom_meta_fields as $field) {
+        $old = get_post_meta($post_id, $field['id'], true);
+        $new = $_POST[$field['id']];
+        if ($new && $new != $old) {
+            update_post_meta($post_id, $field['id'], $new);
+        } elseif ('' == $new && $old) {
+            delete_post_meta($post_id, $field['id'], $old);
+        }
+    } // end foreach
+}
+
 ?>
