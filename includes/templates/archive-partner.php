191a86e1176351bdbf23bcbbdc0b1429a7d09b1a
XicoOfficial <xicoofficial@gmail.com>
2016-03-15 12:21:09 -0600

    update archive and sinble template with the default files on twentisixteen theme

--- includes/templates/archive-partner.php
+++ includes/templates/archive-partner.php
@@ -1,34 +1,101 @@
 <?php
- /*Template Name: New Template
+/**
+ * The template for displaying archive pages
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * If you'd like to further customize these archive views, you may create a
+ * new template file for each one. For example, tag.php (Tag archives),
+ * category.php (Category archives), author.php (Author archives), etc.
  */
+
 get_header(); ?>
-This is the archive page
-<div id="primary">
-    <div id="content" role="main">
-    <?php
-    $mypost = array( 'post_type' => 'partner', );
-    $loop = new WP_Query( $mypost );
-    ?>
-    <?php while ( $loop->have_posts() ) : $loop->the_post();?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header class="entry-header">
 
-                <!-- Display featured image in right-aligned floating div -->
-                <div style="float: right; margin: 10px">
-                    <?php the_post_thumbnail( array( 100, 100 ) ); ?>
-                </div>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-                <!-- Display Title and Author Name -->
-                <strong>Title: </strong><?php the_title(); ?><br />
+		<?php if ( have_posts() ) : ?>
 
-            </header>
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
 
-            <!-- Display movie review contents -->
-            <div class="entry-content"><?php the_content(); ?></div>
-        </article>
+			<?php
+			// Start the Loop.
+			while ( have_posts() ) : the_post(); ?>
 
-    <?php endwhile; ?>
-    </div>
-</div>
-<?php wp_reset_query(); ?>
+				<article id="partner-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
+							<span class="sticky-post"><?php _e( 'Featured'); ?></span>
+						<?php endif; ?>
+
+						<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+					</header><!-- .entry-header -->
+
+					<?php the_excerpt(); ?>
+
+					<?php the_post_thumbnail(); ?>
+
+					<div class="entry-content">
+						<?php
+							/* translators: %s: Name of current post */
+							the_content( sprintf(
+								__( 'Continue reading<span class="screen-reader-text"> "%s"</span>'),
+								get_the_title()
+							) );
+
+							wp_link_pages( array(
+								'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:') . '</span>',
+								'after'       => '</div>',
+								'link_before' => '<span>',
+								'link_after'  => '</span>',
+								'pagelink'    => '<span class="screen-reader-text">' . __( 'Page' ) . ' </span>%',
+								'separator'   => '<span class="screen-reader-text">, </span>',
+							) );
+						?>
+					</div><!-- .entry-content -->
+
+					<footer class="entry-footer">
+						<?php
+							edit_post_link(
+								sprintf(
+									/* translators: %s: Name of current post */
+									__( 'Edit<span class="screen-reader-text"> "%s"</span>'),
+									get_the_title()
+								),
+								'<span class="edit-link">',
+								'</span>'
+							);
+						?>
+					</footer><!-- .entry-footer -->
+				</article><!-- #post-## -->
+
+
+		<?php
+			// End the loop.
+			endwhile;
+
+			// Previous/next page navigation.
+			the_posts_pagination( array(
+				'prev_text'          => __( 'Previous page'),
+				'next_text'          => __( 'Next page'),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page') . ' </span>',
+			) );
+
+		// If no content, include the "No posts found" template.
+		else :
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- includes/templates/single-partner.php
+++ includes/templates/single-partner.php
@@ -1,34 +1,94 @@
 <?php
- /*Template Name: New Template
+/**
+ * The template for displaying all single posts and attachments
+ *
  */
+
 get_header(); ?>
-This is the single page
-<div id="primary">
-    <div id="content" role="main">
-    <?php
-    $mypost = array( 'post_type' => 'partner', );
-    $loop = new WP_Query( $mypost );
-    ?>
-    <?php while ( $loop->have_posts() ) : $loop->the_post();?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header class="entry-header">
 
-                <!-- Display featured image in right-aligned floating div -->
-                <div style="float: right; margin: 10px">
-                    <?php the_post_thumbnail( array( 100, 100 ) ); ?>
-                </div>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <?php
+        // Start the loop.
+        while ( have_posts() ) : the_post(); ?>
 
-                <!-- Display Title and Author Name -->
-                <strong>Title: </strong><?php the_title(); ?><br />
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                </header><!-- .entry-header -->
 
-            </header>
+                <?php twentysixteen_excerpt(); ?>
 
-            <!-- Display movie review contents -->
-            <div class="entry-content"><?php the_content(); ?></div>
-        </article>
+                <?php twentysixteen_post_thumbnail(); ?>
 
-    <?php endwhile; ?>
-    </div>
-</div>
-<?php wp_reset_query(); ?>
+                <div class="entry-content">
+                    <?php
+                        the_content();
+
+                        wp_link_pages( array(
+                            'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:') . '</span>',
+                            'after'       => '</div>',
+                            'link_before' => '<span>',
+                            'link_after'  => '</span>',
+                            'pagelink'    => '<span class="screen-reader-text">' . __( 'Page') . ' </span>%',
+                            'separator'   => '<span class="screen-reader-text">, </span>',
+                        ) );
+
+                        if ( '' !== get_the_author_meta( 'description' ) ) {
+                            get_template_part( 'template-parts/biography' );
+                        }
+                    ?>
+                </div><!-- .entry-content -->
+
+                <footer class="entry-footer">
+                    <?php twentysixteen_entry_meta(); ?>
+                    <?php
+                        edit_post_link(
+                            sprintf(
+                                /* translators: %s: Name of current post */
+                                __( 'Edit<span class="screen-reader-text"> "%s"</span>'),
+                                get_the_title()
+                            ),
+                            '<span class="edit-link">',
+                            '</span>'
+                        );
+                    ?>
+                </footer><!-- .entry-footer -->
+            </article><!-- #post-## -->
+
+<?php
+
+            // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) {
+                comments_template();
+            }
+
+            if ( is_singular( 'attachment' ) ) {
+                // Parent post navigation.
+                the_post_navigation( array(
+                    'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
+                ) );
+            } elseif ( is_singular( 'partner' ) ) {
+                // Previous/next post navigation.
+                the_post_navigation( array(
+                    'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
+                        '<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
+                        '<span class="post-title">%title</span>',
+                    'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
+                        '<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
+                        '<span class="post-title">%title</span>',
+                ) );
+            }
+
+            // End of the loop.
+        endwhile;
+        ?>
+
+    </main><!-- .site-main -->
+
+    <?php get_sidebar( 'content-bottom' ); ?>
+
+</div><!-- .content-area -->
+
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
